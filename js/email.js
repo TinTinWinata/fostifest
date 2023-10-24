a2756002d445cc7b8c08288695a8753e8d462d57
TinTinWinata <86914748+TinTinWinata@users.noreply.github.com>
2023-10-25 00:00:06 +0700

    feat: add comment at email

--- js/email.js
+++ js/email.js
@@ -1,6 +1,7 @@
 const emailDiv = document.getElementById('email');
 const hardwareDivContent = document.getElementById('hardwareContent');
 
+// Make email copied when email icon was clicked
 emailDiv.addEventListener('click', () => {
   navigator.clipboard.writeText('tintin6892@gmail.com');
   hardwareDivContent.innerHTML = 'Email Copied to Clipboard!';
